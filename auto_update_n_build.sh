44f16ef4e62384d1db0f060566cf0f2c3f7f7872
Dpalme <diegoPalmerin24@gmail.com>
2023-03-21 18:35:36 -0600

    auto update script

---
+++ auto_update_n_build.sh
@@ -0,0 +1,8 @@
+cd ~/apps/labrujulaaudiovisual_opalstack_com_http/production/
+INITIAL_ID=$(git rev-parse HEAD)
+git pull origin main
+POSTUPDATE_ID=$(git rev-parse HEAD)
+
+[ $INITIAL_ID = $POSTUPDATE_ID ] && exit 0
+
+npm run build
