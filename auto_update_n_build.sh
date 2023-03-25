2b953cb4cf1678212cc872ecd54f1ace47292e75
osuser <labrujula@vps179.opalstack.com>
2023-03-25 16:00:01 +0000

    manual changes

--- auto_update_n_build.sh
+++ auto_update_n_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd ~/apps/labrujulaaudiovisual_opalstack_com_http/production/
 INITIAL_ID=$(git rev-parse HEAD)
+git add -A
+git commit -m "manual changes"
+git pull origin main
+git push origin main
 git pull origin main
 POSTUPDATE_ID=$(git rev-parse HEAD)
 
