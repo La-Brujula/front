34410018decd5fe03a2655458aaff9420dd04d2d
Dpalme <diegoPalmerin24@gmail.com>
2023-03-28 16:37:25 -0600

    add social links on new page

--- src/shared/components/socials.jsx
+++ src/shared/components/socials.jsx
@@ -6,28 +6,28 @@ export function SocialLinks() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-row gap-8 mx-auto">
-        <a href={contactInformation.facebook}>
+        <a target="_blank" href={contactInformation.facebook}>
           <img
             src={import.meta.env.BASE_URL + 'img/FBIcon.svg'}
             alt="Facebook"
             className="h-10"
           />
         </a>
-        <a href={contactInformation.instagram}>
+        <a target="_blank" href={contactInformation.instagram}>
           <img
             src={import.meta.env.BASE_URL + 'img/IGIcon.svg'}
             alt="Instagram"
             className="h-10"
           />
         </a>
-        <a href={contactInformation.youtube}>
+        <a target="_blank" href={contactInformation.youtube}>
           <img
             src={import.meta.env.BASE_URL + 'img/YoutubeIcon.svg'}
             alt="Youtube"
             className="h-10"
           />
         </a>
-        <a href={contactInformation.whatsapp}>
+        <a target="_blank" href={contactInformation.whatsapp}>
           <img
             src={import.meta.env.BASE_URL + 'img/WAIcon.svg'}
             alt="WhatsApp"
