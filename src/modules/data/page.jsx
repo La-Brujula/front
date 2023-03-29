64dd8c10486ae65caedb9ee956a3176ac8bb9266
osuser <labrujula@vps179.opalstack.com>
2023-03-29 18:20:05 +0000

    manual changes

--- src/modules/data/page.jsx
+++ src/modules/data/page.jsx
@@ -2,6 +2,8 @@ import { Container } from '@shared/layout/container';
 
 import areas from '@shared/constants/areas.json';
 
+window.areas = areas;
+
 export function DataPage() {
   return (
     <>
@@ -14,7 +16,15 @@ export function DataPage() {
           <select onChange={e => {
             console.log('e.target.value', e.target.value);
           }}>
-
+            {(() => {
+              return Object.entries(areas).map(v => {
+                return [v[0], Object.entries(v[1]).map(_v => {
+                  return [_v[0], Object.entries(_v[1])[0][0].split(/-/)[0]];
+                })];
+              }).map(v => v[1]).flat().map(v => {
+                return <option value={v[1]}>{v[0]}</option>;
+              });
+            })()}
           </select>
           <p>
             {'Something something'}
