c41e8158e95bbf025060e2c5a15364fe4b16dc8d
osuser <labrujula@vps179.opalstack.com>
2023-04-03 22:00:01 +0000

    manual changes

--- src/modules/landing/components/heroSection.jsx
+++ src/modules/landing/components/heroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
   const { t } = useTranslation('landing');
   return (
     <Container bg="primary">
-      <div className="flex flex-col lg:flex-row gap-4 lg:gap-16 text-left text-white pb-2 pt-0 xl:py-8 items-center">
+      <div className="flex flex-col lg:flex-row gap-4 lg:gap-16 text-justify text-white pb-2 pt-0 xl:py-8 items-center ">
         <div className="grow w-full flex flex-col gap-4 lg:gap-12">
           <h1 className="font-bold text-3xl md:text-6xl lg:text-5xl leading-tight">
             La Brújula.
@@ -32,9 +32,9 @@ function HeroSection() {
         <p className="grow w-full font-medium text-lg text-justify">
           Durante 13 años nos hemos dedicado a reunir a las personas, empresas e
           instituciones del medio audiovisual y cinematográfico de México para
-          que las puedas encontrar en un solo lugar,
+          que las puedas encontrar en un solo lugar.
           <br />
-          <span className="text-blue font-semibold  block text-center">
+          <span className="text-blue font-semibold block text-center">
             ¡En La Brújula seguimos construyendo!
           </span>
         </p>
