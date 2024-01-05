ef6e330d6688f0e41676a05fe054488afee99ad8
Dpalme <diegoPalmerin24@gmail.com>
2024-01-05 17:59:24 -0600

    fix: Ajustar tamaño de fotos para testimonios #3

--- src/modules/landing/components/quoteSlide.jsx
+++ src/modules/landing/components/quoteSlide.jsx
@@ -1,13 +1,15 @@
 function QuoteSlide({ imageUrl, name, quote, title }) {
   return (
     <div
-      className="grid grid-cols-1  lg:grid-cols-[2fr_3fr] gap-4 lg:gap-8 max-w-4xl
-      mx-auto translate-x-[var(--carusel-animation)] items-center
+      className="grid grid-cols-1  lg:grid-cols-[2fr_3fr]
+      grid-rows-[min-content_1fr] lg:grid-rows-1 gap-2 lg:gap-8 max-w-4xl
+      mx-auto translate-x-[var(--carusel-animation)] items-center object-bottom lg:object-center
       transition-transform duration-400 py-8 h-full"
     >
       <img
         src={`${import.meta.env.BASE_URL}img${imageUrl}`}
-        className="w-40 fit-content rounded-md h-40 object-scale-down object-fit object-center"
+        className="w-1/2 fit-content rounded-md h-auto object-contain
+        object-bottom lg:object-center max-h-[24rem]"
       />
       <div className="relative text-primary w-full text-left">
         <h3 className="font-bold text-lg">{name}</h3>
