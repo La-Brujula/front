6d1ed4ec93456a1b499141e1fc0f6a6d315f56bf
osuser <labrujula@vps179.opalstack.com>
2024-01-05 19:43:55 +0000

    manual changes

--- auto_update_n_build.sh
+++ auto_update_n_build.sh
@@ -3,9 +3,9 @@ cd ~/apps/labrujulaaudiovisual_opalstack_com_http/production/
 INITIAL_ID=$(git rev-parse HEAD)
 git add -A
 git commit -m "manual changes"
-git pull origin main
-git push origin main
-git pull origin main
+git pull ssh main
+git push ssh main
+git pull ssh main
 POSTUPDATE_ID=$(git rev-parse HEAD)
 
 [ $INITIAL_ID = $POSTUPDATE_ID ] && exit 0
