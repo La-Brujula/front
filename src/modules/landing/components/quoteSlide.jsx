eccbf24ceccc4059ce91b457dd641e807fb6169f
Dpalme <diegoPalmerin24@gmail.com>
2024-01-05 14:19:21 -0600

    fix: Quitar comillas de testimonios #4

--- src/modules/landing/components/quoteSlide.jsx
+++ src/modules/landing/components/quoteSlide.jsx
@@ -5,21 +5,14 @@ function QuoteSlide({ imageUrl, name, quote, title }) {
       mx-auto translate-x-[var(--carusel-animation)] items-center
       transition-transform duration-400 py-8 h-full"
     >
-<img
+      <img
         src={`${import.meta.env.BASE_URL}img${imageUrl}`}
         className="w-40 fit-content rounded-md h-40 object-scale-down object-fit object-center"
       />
-      <div className="grid gap-2 grid-cols-[2rem_1fr] self-start"> 
-        <img
-          src={`${import.meta.env.BASE_URL}img/Apostrofe.svg`}
-          className="h-8 self-start"
-          loading="lazy"
-        />
-        <div className="relative text-primary w-full text-left">
-          <h3 className="font-bold text-lg">{name}</h3>
-          <p className="font-normal text-sm mb-8 ">{title}</p>
-          <p className="grow">{quote}</p>
-        </div>
+      <div className="relative text-primary w-full text-left">
+        <h3 className="font-bold text-lg">{name}</h3>
+        <p className="font-normal text-sm mb-8 ">{title}</p>
+        <p className="grow">{quote}</p>
       </div>
     </div>
   );
