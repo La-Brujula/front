4c22e1f37726db2ee507dbb7dc0ef4dedfa9a943
Dannnnn-afk <114628250+Dannnnn-afk@users.noreply.github.com>
2024-01-30 18:53:10 -0600

    Nombres de áreas y pendientes. (#23)
    * Nombres de áreas y pendientes.
    Cambios pendientes de imagen azul de 14 años y cambios en las palabras de staff y crew.

--- src/modules/about/components/hero.jsx
+++ src/modules/about/components/hero.jsx
@@ -26,11 +26,11 @@ export function AboutHero() {
               alt={t('mexicoDestinoDeFilmacion')}
               className="h-16 lg:h-20"
             />
-            {/* <img
-              src={import.meta.env.BASE_URL + '}'}
-              alt={t('')}
+             <img
+              src={import.meta.env.BASE_URL + 'img/Logo14añosazul.svg'}
+              alt={t('logo14años')}
               className="h-20 lg:h-32"
-            /> */}
+            /> 
           </div>
         </div>
         <p className='text-justify'>
