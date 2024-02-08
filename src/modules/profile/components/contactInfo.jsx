aa459bff7864970ca8cf0961f9da14557643afd4
Dpalme <diegoPalmerin24@gmail.com>
2024-02-07 23:04:49 -0600

    don't show user email for moral if another one is registered

--- src/modules/profile/components/contactInfo.jsx
+++ src/modules/profile/components/contactInfo.jsx
@@ -61,7 +61,7 @@ export const ContactSection = ({ user }) => {
           </div>
         </>
       )}
-      {!!user.email && (
+      {!!user.email && !(user.type == 'moral' && user.altEmail) && (
         <>
           <h3>
             <EmailOutlined />
