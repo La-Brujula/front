de8ea32c6e37935896f9bb297f46157378986e07
Dpalme <diegoPalmerin24@gmail.com>
2024-02-15 00:16:48 -0600

    silenced use client build errors

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
-import path from 'path'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      /**
+       * Ignore "use client" waning since we are not using SSR
+       * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
+       */
+      onwarn(warning, warn) {
+        if (
+          warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+          warning.message.includes(`"use client"`)
+        ) {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   server: {
-    port: 12453
+    port: 12453,
   },
   resolve: {
     alias: {
-      '@assets':  path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src/assets'),
-      '@modules': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src/modules'),
-      '@shared':  path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src/shared'),
-      '@':        path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
-    }
-  }
-})
+      '@assets': path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        'src/assets'
+      ),
+      '@modules': path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        'src/modules'
+      ),
+      '@shared': path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        'src/shared'
+      ),
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+    },
+  },
+});
