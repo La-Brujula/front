626bb1584d99caaa9d17d83ae7a23bdf0f0eee3a
Dpalme <diegoPalmerin24@gmail.com>
2024-02-15 20:54:16 -0600

    disable manual changes on prod

--- auto_update_n_build.sh
+++ auto_update_n_build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 cd ~/apps/labrujulaaudiovisual_opalstack_com_http/production/
 INITIAL_ID=$(git rev-parse HEAD)
-git add -A
-git commit -m "manual changes"
-git pull ssh main
-git push ssh main
-git pull ssh main
+git fetch ssh
+git reset --hard ssh/main
 POSTUPDATE_ID=$(git rev-parse HEAD)
 
 [ $INITIAL_ID = $POSTUPDATE_ID ] && exit 0
