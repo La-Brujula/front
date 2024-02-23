d22e47badd4de5339cca74c7b923c4d001200a65
Dpalme <diegoPalmerin24@gmail.com>
2024-02-22 21:11:47 -0600

    removed typos from areas

--- src/modules/landing/components/quoteSlide.tsx
+++ src/modules/landing/components/quoteSlide.tsx
@@ -11,7 +11,7 @@ function QuoteSlide({
 }) {
   return (
     <div
-      className="grid grid-cols-1  lg:grid-cols-[2fr_3fr]
+      className="grid grid-cols-1 lg:grid-cols-[2fr_3fr]
       grid-rows-[min-content_1fr] lg:grid-rows-1 gap-2 lg:gap-8 max-w-4xl
       mx-auto translate-x-[var(--carusel-animation)] items-center object-bottom lg:object-center
       transition-transform duration-400 py-8 h-full"
