254a4e0192f97f4e0a9594c30f647a9e0accde53
Sebastian Barrio <sebastian.barrio.b@gmail.com>
2024-02-29 14:59:44 -0600

    67 refactorizar módulo about para usar i18n (#68)
    * Sustituir texto plano por llamadas a la funcion de traduccion de i18n con configuracion 'about'
    
    * Locale updating
    
    * Fix multiline and dynamic translation problems
    
    * Locale changes
    
    * fix hero not being translated

--- src/modules/about/components/hero.tsx
+++ src/modules/about/components/hero.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@shared/layout/container';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 export function AboutHero() {
   const { t } = useTranslation('about');
@@ -12,13 +12,18 @@ export function AboutHero() {
       >
         <div>
           <h2>
-            La Brújula.
-            <br />
-            Red de la industria <br />
-            audiovisual y<br />
-            cinematográfica
-            <br />
-            de México.
+            <Trans
+              i18nKey="heroTitle"
+              t={t}
+            >
+              La Brújula.
+              <br />
+              Red de la industria <br />
+              audiovisual y<br />
+              cinematográfica
+              <br />
+              de México.
+            </Trans>
           </h2>
           <div className="flex flex-row gap-4 lg:gap-12 items-center">
             <img
@@ -34,22 +39,29 @@ export function AboutHero() {
           </div>
         </div>
         <p className="text-justify">
-          La Brújula se fundó en 2010 en la ciudad de Guadalajara teniendo como
-          antecedente “El Medio”, ambas publicaciones inspiradas por directorios
-          especializados en la producción cinematográfica y televisiva en Los
-          Ángeles, Nueva York, Toronto y Montreal entre otros.
-          <br />
-          <br />
-          Nuestro equipo involucra: estrategia, ciencia de datos, tecnologías de
-          la información, captura de datos, diseño grafico y editorial,
-          traducción, diseño web, vinculación, publicidad, logística,
-          distribución, manejo de redes sociales y atención a usuarios.
-          <br />
-          <br />
-          Confiamos en el ejercicio de la vinculación, la creación de redes, la
-          gestión de encuentros que nos llevan a hacer comunidad y gestionar
-          proyectos que ofrecen un valor cultural y que generan fuentes de
-          empleo y oportunidades de negocio para todos los involucrados.
+          <Trans
+            i18nKey="heroParagraph"
+            t={t}
+          >
+            La Brújula se fundó en 2010 en la ciudad de Guadalajara teniendo
+            como antecedente “El Medio”, ambas publicaciones inspiradas por
+            directorios especializados en la producción cinematográfica y
+            televisiva en Los Ángeles, Nueva York, Toronto y Montreal entre
+            otros.
+            <br />
+            <br />
+            Nuestro equipo involucra estrategia, ciencia de datos, tecnologías
+            de la información, captura de datos, diseño grafico y editorial,
+            traducción, diseño web, vinculación, publicidad, logística,
+            distribución, manejo de redes sociales y atención a usuarios.
+            <br />
+            <br />
+            Confiamos en el ejercicio de la vinculación, la creación de redes,
+            la gestión de encuentros que nos llevan a hacer comunidad y
+            gestionar proyectos que ofrecen un valor cultural y que generan
+            fuentes de empleo y oportunidades de negocio para todos los
+            involucrados.
+          </Trans>
         </p>
       </div>
     </Container>
