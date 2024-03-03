5e5e3f32f591a5deba7c8f508e9592842c498fd0
Sebastian Barrio <sebastian.barrio.b@gmail.com>
2024-03-03 16:34:29 -0600

    Add 404 page

---
+++ src/shared/navigation/page404.tsx
@@ -0,0 +1,35 @@
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+
+export default () => {
+  const { t } = useTranslation();
+  return (
+    <div className="bg-secondary h-lvh bg-opacity-20 gap-10 flex flex-col items-center pt-32 pb-16 pl-8 pr-8">
+      <div className="flex flex-col items-center gap-8 z-10">
+        <div className=" items-center flex flex-col gap-4">
+          <h1 className="text-5xl">{t('Esta página no existe')}</h1>
+          <p className="text-lg">{t('Sentimos las molestias')}</p>
+        </div>
+        <div className="flex flex-row justify-center gap-8">
+          <NavLink
+            to="javascript:history.back()"
+            className="button bg-transparent text-current h-11"
+          >
+            {t('Regresar')}
+          </NavLink>
+          <NavLink
+            to="/"
+            className="button h-11"
+          >
+            {t('Página principal')}
+          </NavLink>
+        </div>
+      </div>
+      <img
+        src={import.meta.env.BASE_URL + 'img/HalfLogo.svg'}
+        alt=""
+        className="absolute opacity-20 top-24 -translate-x-1/2 rotate-180 left-1/2 w-5/12"
+      />
+    </div>
+  );
+};
