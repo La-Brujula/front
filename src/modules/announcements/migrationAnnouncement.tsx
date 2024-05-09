7d6757dc9f51e05d967d59ebc5565b98cccbd181
Dpalme <diegoPalmerin24@gmail.com>
2024-05-08 19:56:18 -0600

    added migration announcement

---
+++ src/modules/announcements/migrationAnnouncement.tsx
@@ -0,0 +1,66 @@
+import { Link } from '@tanstack/react-router';
+import { Trans, useTranslation } from 'react-i18next';
+
+function MigrationAnnouncement() {
+  const { t } = useTranslation('migrationAnnouncement');
+  return (
+    <div className="">
+      <h2>{t('La Brújula 2024')}</h2>
+      <div className="text-black max-w-2xl mx-auto text-justify [text-align-last:center] *:mb-4">
+        <p>
+          <Trans
+            t={t}
+            i18nKey="EstrenamosVersion"
+          >
+            Estrenamos la versión 2024 de la plataforma que incluye búsquedas
+            más rápidas y precisas, optimizaciones de desempeño y la opción de
+            usar la plataforma en inglés.
+          </Trans>
+        </p>
+        <p>
+          <Trans
+            t={t}
+            i18nKey="HemosTrabajado"
+          >
+            Hemos trabajado en el desarrollo de nuevas funcionalidades para
+            aumentar la seguridad y protección de los datos de nuestra
+            comunidad. Es por eso que es muy importante actualizar tus datos de
+            usuario y cambiar tu contraseña para que cumpla con los nuevos
+            estándares de seguridad.
+          </Trans>
+        </p>
+        <p>
+          <Trans
+            t={t}
+            i18nKey="HacerloTeTomara"
+          >
+            Hacerlo te tomará solo un par de minutos en esta liga:
+          </Trans>
+        </p>
+        <p>
+          <Link to="/auth/reset-password">
+            <button>{t('Reiniciar contraseña')}</button>
+          </Link>
+        </p>
+        <p>
+          <Trans
+            t={t}
+            i18nKey="GraciasPor"
+          >
+            ¡Gracias por contribuir a la comunidad audiovisual del país!
+          </Trans>
+        </p>
+        <p>
+          <Trans
+            t={t}
+            i18nKey="Recibe un saludo"
+          >
+            Recibe un saludo del equipo de labrujula.com.mx
+          </Trans>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MigrationAnnouncement;
--- src/routes/announcements/index.lazy.tsx
+++ src/routes/announcements/index.lazy.tsx
@@ -1,7 +1,8 @@
+import MigrationAnnouncement from '@/modules/announcements/migrationAnnouncement';
 import guides from '@shared/constants/guides.json';
 import { Container } from '@shared/layout/container';
 import { createLazyFileRoute } from '@tanstack/react-router';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 export const Route = createLazyFileRoute('/announcements/')({
   component: AnnouncementsPage,
@@ -17,91 +18,22 @@ function AnnouncementsPage() {
         className="text-white"
       >
         <h2>
-          La Brújula.
-          <br />
-          Red de la industria <br />
-          audiovisual y cinematográfica
-          <br />
-          de México.
+          <Trans
+            t={t}
+            i18nKey="brujulaHeadline"
+          >
+            La Brújula.
+            <br />
+            Red de la industria audiovisual y cinematográfica de México.
+          </Trans>
         </h2>
-        <div className="flex flex-row gap-4 lg:gap-12 items-center justify-center">
-          <img
-            src={import.meta.env.BASE_URL + 'img/MexicoDestFilm.svg'}
-            alt={t('mexicoDestinoDeFilmacion')}
-            className="h-16 lg:h-20"
-          />
-          <img
-            src={import.meta.env.BASE_URL + 'img/Logo14anos.svg'}
-            alt={t('14YearsBruj')}
-            className="h-20 lg:h-32"
-          />
-        </div>
       </Container>
       <Container className="text-primary">
         <div className="max-w-3xl mx-auto mb-8">
-          <h2>
-            {t('Descarga La Brújula en PDF para utilizarla sin conexión')}
-          </h2>
-          <p>
-            {t(
-              'La Brújula audiovisual en documentos interactivos, te permiten hacer contacto rápidamente con la persona, empresa, servicio o institución que estas buscando.'
-            )}
-          </p>
+          <h1>{t('Anuncios')}</h1>
+          <p>{t('Mantente al día sobre lo que pasa en La Brújula')}</p>
         </div>
-        <h3
-          className="border-b border-primary text-left
-        font-normal text-xl"
-        >
-          {t('Ediciones regionales')}
-        </h3>
-        <div
-          className="grid grid-cols-[repeat(auto-fit,_minmax(12rem,_1fr))] gap-8
-        py-8"
-        >
-          {guides.regional.map((guide) => (
-            <a
-              key={guide.name}
-              href={guide.link}
-              className="overflow-hidden rounded-md max-w-xs"
-              download
-            >
-              <img
-                src={guide.thumbnail}
-                alt={guide.name}
-                className="w-full"
-              />
-            </a>
-          ))}
-        </div>
-        <h3
-          className="border-b border-primary text-left
-        font-normal text-xl"
-        >
-          {t('Ediciones especiales')}
-        </h3>
-        <div
-          className="grid grid-cols-[repeat(auto-fit,_minmax(12rem,_1fr))] gap-8
-        py-8"
-        >
-          {guides.specials.map((guide, i) => (
-            <a
-              href={guide.link}
-              className="overflow-hidden rounded-md max-w-xs"
-              download
-              key={guide.name + i}
-            >
-              <img
-                src={guide.thumbnail}
-                alt={guide.name}
-                className="w-full"
-              />
-            </a>
-          ))}
-          {/* Added to preserve sizing with the above guides */}
-          <div className=""></div>
-          <div className=""></div>
-          <div className=""></div>
-        </div>
+        <MigrationAnnouncement />
       </Container>
     </>
   );
