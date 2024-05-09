ee21ea4b624b0aeec0c4257ca9b8a9735ab7dc1f
Diego Palmerin <57266493+Dpalme@users.noreply.github.com>
2024-05-09 13:23:44 -0600

    removed relative urls from html (#103)

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: plugins,
-    base: '',
+    base: '/',
     resolve: {
       alias: {
         '@assets': path.resolve(
