43c718b80d4f82c62cb7db7dd3df70c18b25136c
Dpalme <diegoPalmerin24@gmail.com>
2024-09-03 17:06:31 -0600

    left-align download guides title

--- src/modules/landing/components/downloadGuides.tsx
+++ src/modules/landing/components/downloadGuides.tsx
@@ -68,7 +68,7 @@ function DownloadGuides() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 mt-16 gap-4">
         <div className="text-left flex md:order-last flex-row justify-end w-full">
-          <div className="md:max-w-lg text-center sm:text-left">
+          <div className="md:max-w-lg lg:text-center sm:text-left">
             <h2 className="">
               <Trans
                 i18nKey="specialGuides"
