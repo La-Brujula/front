4d563a9f0a5625ec3c376c14eb3eabc441eb3a5d
Dpalme <diegoPalmerin24@gmail.com>
2024-12-05 11:16:07 -0600

    fixed email verification flow race-conditions

--- src/routes/auth/verify-email.lazy.tsx
+++ src/routes/auth/verify-email.lazy.tsx
@@ -1,7 +1,4 @@
-import { verifyEmail } from '@/modules/auth/hooks/emailVerification';
-import DataSuspense from '@/shared/components/dataSuspense';
-import { ErrorMessage } from '@/shared/components/errorMessage';
-import { useCurrentProfile } from '@/shared/hooks/useCurrentProfile';
+import { useLoggedInAccount } from '@/shared/hooks/useLoggedInAccount';
 import { Container } from '@/shared/layout/container';
 import { createLazyFileRoute, Link, useNavigate } from '@tanstack/react-router';
 import React, { useEffect } from 'react';
@@ -12,44 +9,34 @@ export const Route = createLazyFileRoute('/auth/verify-email')({
 });
 
 function VerifyEmail() {
-  const { code, email } = Route.useSearch();
   const { t } = useTranslation('auth');
-  const { data: user, isLoading, error } = useCurrentProfile();
+  const account = useLoggedInAccount();
   const navigate = useNavigate();
   useEffect(() => {
-    if (!!user?.id) {
-      verifyEmail(code, email).then((verifyRes) => {
-        if (verifyRes.isSuccess) {
-          navigate({ to: '/profile/$userId', params: { userId: 'me' } });
-        }
-      });
-    }
-  }, [user, code, navigate]);
+    const navigateCallback = setTimeout(
+      () =>
+        account !== null
+          ? navigate({ to: '/profile/$userId', params: { userId: 'me' } })
+          : navigate({ to: '/auth/login' }),
+      2000
+    );
+    return () => clearTimeout(navigateCallback);
+  }, [account, navigate]);
 
   return (
     <Container
       bg="light"
       className="min-h-96"
     >
-      <DataSuspense
-        loading={isLoading}
-        error={error}
-        fallback={<h1>{t('Cargando...')}</h1>}
-      >
-        {user?.verified ? (
-          <div className="bg-emerald-500 bg-opacity-25 border border-emerald-500">
-            <h2>{t('Se verificó tu correo existósamente')}</h2>
-            <Link
-              to="/profile/$userId"
-              params={{ userId: 'me' }}
-            >
-              <p>{t('Haz click aquí para regresar a tu perfil')}</p>
-            </Link>
-          </div>
-        ) : (
-          <ErrorMessage message="AE-VE" />
-        )}
-      </DataSuspense>
+      <div className="bg-emerald-500 bg-opacity-25 border border-emerald-500">
+        <h2>{t('Se verificó tu correo existósamente')}</h2>
+        <Link
+          to={account !== null ? '/profile/$userId' : '/auth/login'}
+          params={{ userId: account !== null ? 'me' : undefined }}
+        >
+          <p>{t('Haz click aquí para regresar a tu perfil')}</p>
+        </Link>
+      </div>
     </Container>
   );
 }
